a939444ce6e79dbacd058bd1c8706820a8bc5b83
Miqueias dos Santos Sousa Assad <misousa@padtec.com.br>
2021-06-09 09:51:04 -0300

    [VUE] usando diretiva v-for

--- curso-vue/alurapic/src/App.vue
+++ curso-vue/alurapic/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
-    <img v-bind:src="foto.url" :alt="foto.titulo">
+    <ul>
+      <li v-for="foto of fotos" 
+          :key="foto.titulo">
+            <img v-bind:key="foto.titulo" v-bind:src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,10 +15,16 @@ export default {
   data() {
     return {
       titulo: 'AluraPic',
-      foto: {
-        url: 'https://s2.glbimg.com/slaVZgTF5Nz8RWqGrHRJf0H1PMQ=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/e/NTegqdSe6SoBAoQDjKZA/cachorro.jpg',
-        titulo: 'cachorro'
-      }
+      fotos: [
+        {
+          url: 'https://s2.glbimg.com/slaVZgTF5Nz8RWqGrHRJf0H1PMQ=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/e/NTegqdSe6SoBAoQDjKZA/cachorro.jpg',
+          titulo: 'cachorro'
+        },
+        {
+          url: 'https://s2.glbimg.com/slaVZgTF5Nz8RWqGrHRJf0H1PMQ=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/e/NTegqdSe6SoBAoQDjKZA/cachorro.jpg',
+          titulo: 'cachorão'
+        }
+      ]
     }
   },
 }
