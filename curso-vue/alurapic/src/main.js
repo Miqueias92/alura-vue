fbc22bbc72e6b61c0bbc72dc8557754abb40a696
Miqueias Sousa <mi.queias2010@hotmail.com>
2021-08-21 23:21:52 -0300

    [VUE] configurando as rotas da aplicação

--- curso-vue/alurapic/src/main.js
+++ curso-vue/alurapic/src/main.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 import VueResource from 'vue-resource';
 import vueRouter from 'vue-router';
+import { routes } from './routes';
 
 Vue.use(VueResource);
 Vue.use(vueRouter);
 
+const router = new vueRouter({ 
+  routes,
+  mode: "history", // remove a # da barra de navegação 
+}) 
+
+// localhot:8080/#/  | localhot:8080/#/cadastro
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
